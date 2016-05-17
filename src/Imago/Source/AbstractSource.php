bb2bb5bdcde5b57f018d6d99bb4f2ea068eaa4b2
Olaf Schneider <mail@olafschneider.de>
2016-05-17 22:25:34 +0200

    moved option methods into a trait to keep class source code small and simple

--- src/Imago/Source/AbstractSource.php
+++ src/Imago/Source/AbstractSource.php
@@ -2,49 +2,21 @@
 
 namespace Filchos\Imago\Source;
 
+use Filchos\Imago\OptionTrait;
 use Filchos\Imago\Transformable;
 
 abstract class AbstractSource implements Transformable
 {
 
-    private $options;
+    use OptionTrait;
 
     public function __construct(array $options = [])
     {
-        $this->options = $options;
+        $this->setOptions($options);
     }
 
     public function __invoke()
     {
         return $this->get();
-    }
-
-    public function getOptions()
-    {
-        return $this->options;
-    }
-
-    public function getOption($name, $default = null)
-    {
-        if ($this->hasOption($name)) {
-            return $this->options[$name];
-        } else {
-            return $default;
-        }
-    }
-
-    public function hasOption($name)
-    {
-        return isset($this->options[$name]);
-    }
-
-    public function setOption($name, $value)
-    {
-        $this->options[$name] = $value;
-    }
-
-    public function deleteOption($name)
-    {
-        unset($this->options[$name]);
     }
 }
