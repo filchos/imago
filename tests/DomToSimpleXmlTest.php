b2a9132a723794998ac2cbf6f66c04261ac5037c
Olaf Schneider <mail@olafschneider.de>
2016-05-20 11:55:10 +0200

    tests for HtmlToDom and DomToSimpleXml

--- tests/DomToSimpleXmlTest.php
+++ tests/DomToSimpleXmlTest.php
@@ -4,6 +4,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 use Filchos\Imago\Source\Value;
 use Filchos\Imago\Transformer\DomToSimpleXml;
+use Filchos\Imago\Transformer\HtmlToDom;
 use Filchos\Imago\Transformer\XmlToDom;
 
 class DomToSimpleXmlTest extends PHPUnit_Framework_TestCase
@@ -17,9 +18,22 @@ class DomToSimpleXmlTest extends PHPUnit_Framework_TestCase
         $imago  = new DomToSimpleXml($imago);
         $simple = $imago->get();
         $this->assertInstanceOf('SimpleXMLElement', $simple);
-        $this->assertEquals(3, count($simple->item));
-        $this->assertEquals('Tåme', (string) $simple->item[2]);
-        $this->assertEquals('root', $simple->getName());
+        $this->assertSame(3, count($simple->item));
+        $this->assertEquals('Tåme', $simple->item[2]);
+        $this->assertSame('Tåme', (string) $simple->item[2]);
+        $this->assertSame('root', $simple->getName());
+    }
+
+    public function testUglyHtmlGet()
+    {
+        $html    = '<li>Innervik<li>Yttervik<li>Bureå<li>Burvik';
+        $imago  = new Value($html);
+        $imago  = new HtmlToDom($imago);
+        $imago  = new DomToSimpleXml($imago);
+        $simple = $imago->get();
+        $list   = $simple->xpath('//li');
+        $this->assertEquals(4, count($list));
+        $this->assertEquals('Bureå', $list[2]);
     }
 
     /**
