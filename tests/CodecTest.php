df7cc4443ac1d21e50cca3c48fadd7d3a7d87863
Olaf Schneider <mail@olafschneider.de>
2016-05-23 23:16:51 +0200

    add tests for basic codecs

---
+++ tests/CodecTest.php
@@ -0,0 +1,30 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Filchos\Imago\Cache\JsonCodec;
+use Filchos\Imago\Cache\PhpSerializeCodec;
+
+class CodecTest extends PHPUnit_Framework_TestCase
+{
+
+    public function testJsonCodec()
+    {
+        $raw     = ['city' => 'Svappavaara'];
+        $codec   = new JsonCodec;
+        $encoded = $codec->encode($raw);
+        $this->assertSame('{"city":"Svappavaara"}', $encoded);
+        $decoded = (array) $codec->decode($encoded);
+        $this->assertSame($raw, $decoded);
+    }
+
+    public function testSerializeCodec()
+    {
+        $raw     = ['city' => 'Svappavaara'];
+        $codec   = new PhpSerializeCodec;
+        $encoded = $codec->encode($raw);
+        $this->assertSame('a:1:{s:4:"city";s:11:"Svappavaara";}', $encoded);
+        $decoded = $codec->decode($encoded);
+        $this->assertSame($raw, $decoded);
+    }
+}
