098b90ed925bc9d76a4f5128d9f78e93564d2856
Olaf Schneider <mail@olafschneider.de>
2016-06-19 11:56:09 +0200

    moved polyfill for json_last_error_msg into class method

--- src/Imago/Transformer/JsonDecoder.php
+++ src/Imago/Transformer/JsonDecoder.php
@@ -5,10 +5,6 @@ namespace Filchos\Imago\Transformer;
 use Filchos\Imago\Exception\EmptyException;
 use Filchos\Imago\Exception\JsonException;
 
-if (!function_exists('json_last_error_msg')) {
-    require_once __DIR__ . '/../../polyfills/json_last_error_msg.php';
-}
-
 class JsonDecoder extends AbstractTransformer
 {
 
@@ -25,6 +21,9 @@ class JsonDecoder extends AbstractTransformer
         $output = json_decode($input);
 
         if (json_last_error() != JSON_ERROR_NONE) {
+            if (!function_exists('json_last_error_msg')) {
+                require_once __DIR__ . '/../../polyfills/json_last_error_msg.php';
+            }
             throw new JsonException(json_last_error_msg(), json_last_error());
         }
 
