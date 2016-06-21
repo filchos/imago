76204c5035f0d78745a7b161eed64376b2ad809e
Olaf Schneider <mail@olafschneider.de>
2016-06-21 20:32:31 +0200

    make base Exception abstract

--- src/Imago/Exception/Exception.php
+++ src/Imago/Exception/Exception.php
@@ -1,9 +1,21 @@
 <?php
 
+/**
+ * part of the library filchos/imago
+ *
+ * @package filchos/imago
+ * @author  Olaf Schneider <mail@olafschneider.net>
+ */
+
 namespace Filchos\Imago\Exception;
 
 use Exception as RootException;
 
-class Exception extends RootException
+/**
+ * the abstract parent class for all exceptions in the namespace Filchos\Imago
+ *
+ * @abstract
+ */
+abstract class Exception extends RootException
 {
 }
